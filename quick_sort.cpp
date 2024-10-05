f4c54a4127725ca1eb952aef40aacec929f2d83e
André Marcel Gutiérrez Benítez <68956970+AndreMarcel99@users.noreply.github.com>
2024-10-05 15:33:42 -0600

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,8 +2,12 @@
 #include <cstdlib>
 #include <random>
 #include <chrono>
+#include <cmath>
+#include "matplotlibcpp.h"
+#define _USE_MATH_DEFINES
 using namespace std;
 using namespace std::chrono;
+namespace plt = matplotlibcpp;
 
 int random_number(int max, int min){
     srand(time(NULL));
@@ -93,35 +97,67 @@ int main()
 
     n = sizeof(arr_25) / sizeof(arr_25[0]);
     fill_array(arr_25, n);
-    cout << get_Average(arr_25, n) << "\n";
+    arr_average[aux] = get_Average(arr_25, n);
+    aux++;
 
     n = sizeof(arr_5) / sizeof(arr_5[0]);
     fill_array(arr_5, n);
-    cout << get_Average(arr_5, n) << "\n";
+    arr_average[aux] = get_Average(arr_5, n);
+    aux++;
 
     n = sizeof(arr_75) / sizeof(arr_75[0]);
     fill_array(arr_75, n);
-    cout << get_Average(arr_75, n) << "\n";
+    arr_average[aux] = get_Average(arr_75, n);
+    aux++;
 
     n = sizeof(arr_10) / sizeof(arr_10[0]);
     fill_array(arr_10, n);
-    cout << get_Average(arr_10, n) << "\n";    
+    arr_average[aux] = get_Average(arr_10, n);
+    aux++;  
 
     n = sizeof(arr_12) / sizeof(arr_12[0]);
     fill_array(arr_12, n);
-    cout << get_Average(arr_12, n) << "\n";
+    arr_average[aux] = get_Average(arr_12, n);
+    aux++;  
 
     n = sizeof(arr_15) / sizeof(arr_15[0]);
     fill_array(arr_15, n);
-    cout << get_Average(arr_15, n) << "\n";
+    arr_average[aux] = get_Average(arr_15, n);
+    aux++; 
 
     n = sizeof(arr_17) / sizeof(arr_17[0]);
     fill_array(arr_17, n);
-    cout << get_Average(arr_17, n) << "\n";
+    arr_average[aux] = get_Average(arr_17, n);
+    aux++; 
 
     n = sizeof(arr_2) / sizeof(arr_2[0]);
     fill_array(arr_2, n);
-    cout << get_Average(arr_2, n) << "\n";
-
+    arr_average[aux] = get_Average(arr_2, n);
+    double k1,k2;
+    k1 = arr_average[4] / pow(1000, 2);
+    k2 = arr_average[4] / (1000 * (log(1000)));
+    for(int i=0;i < 9;i++){
+        cout <<arr_sizes[i] << ":" << arr_average[i] <<", ";
+    }
+    vector<double> y = {0,0,0,0,0,0,0,0,0};
+    vector<double> x = {0,0,0,0,0,0,0,0,0};
+    for (int i = 0; i < y.size(); i++){
+        y[i] = arr_average[i];
+        x[i] = arr_sizes[i];
+    }
+    vector<double> x_2(3000), x_log(3000), x_sub(3000);
+    for(int i=0;i<3000; i++){
+        x_2.at(i) = k1 * pow(i, 2);
+        x_log.at(i) = k2 * (i * log(i));
+        x_sub.at(i) = i;
+    }
+    plt::figure_size(1200, 780);
+    plt::plot(x, y, "r--");
+    plt::plot(x_sub, x_2);
+    plt::plot(x_sub, x_log);
+    plt::xlim(0, 3000);
+    plt::ylim(0, 1000);
+    plt::show();
+    plt::save("./basic.png");
     return 0;
 }
